406ad21a8520e68ab412bd13c9a194cfc53b24ba
Gusteivos gp <gc5142387@gmail.com>
2023-09-07 02:40:31 -0300

    removing third_party folder

--- third_party/cloc_auto.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-cloc.exe ./../CMakeLists.txt ./../*.c ./../*.h ./../gameplay_shared/*.c ./../gameplay_shared/*.h ./../gameplay/*.c ./../gameplay/*.h ./../input/*.c ./../input/*.h
--- third_party/cloc_auto.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cloc ./../CMakeLists.txt ./../*.c ./../*.h ./../gameplay_shared/*.c ./../gameplay_shared/*.h ./../gameplay/*.c ./../gameplay/*.h ./../input/*.c ./../input/*.h
